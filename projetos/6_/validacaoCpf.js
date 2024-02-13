ef86827c71ed785976946f089e217d32e6f5bc10
Max Santos <max.santosdev@gmail.com>
2024-02-13 07:29:25 -0300

    Refatorando o exercicio anterior para utilização de prototypes

--- projetos/6_/validacaoCpf.js
+++ projetos/6_/validacaoCpf.js
@@ -5,50 +5,62 @@ function CPFValidator(cpf) {
 
     this.init = () => {
         const cleanCPF = this.cleanCPF(cpf);
-        const validCPF = this.validCpfGenerated(cleanCPF);
-        const confirm = this.confirmCPF(validCPF, cleanCPF);
+        const validCPF = this.validCleanCpf(cleanCPF);
 
-        console.log(confirm ? 'CPF Válido' : 'CPF Inválido');
+        console.log(validCPF ? 'CPF Válido' : 'CPF Inválido');
     }
-
-    this.validCpfGenerated = (cleanCPF) => {
-        const arrayCPF = this.transformedArray(cleanCPF);
-        const calcDigit1 = this.calcCPF(arrayCPF);
-        const digit1 = this.digitCalc(calcDigit1);
-
-        this.addDigit(arrayCPF, String(digit1))
-
-        const calcDigit2 = this.calcCPF(arrayCPF);
-        const digit2 = this.digitCalc(calcDigit2);
-        this.addDigit(arrayCPF, String(digit2))
-
-        const newCpf = this.generateCPF(arrayCPF);
-        return newCpf;
-    }
-
-    this.cleanCPF = (cpf) => cpf.replace(/\D+/g, ''); // Removendo os caractéres que não forem números
-    this.transformedArray = (cleanCPF) => Array.from(cleanCPF).slice(0, -2);
-    this.addDigit = (array, digit) => array.push(digit);
-    this.generateCPF = (array) => array.join('');
-    this.confirmCPF = (validCPF, cpf) => validCPF !== '11111111' && validCPF !== '99999999' && validCPF === cpf;
-
-    this.digitCalc = (result) => {
-        const calculatedDigit = 11 - (result % 11);
-        return (calculatedDigit <= 9) ? calculatedDigit : 0;
-    }
-
-    this.calcCPF = (cpfArray) => {
-        const result = cpfArray.reduce((accumulator, value, index, array) => {
-            const lengthArray = (array.length + 1) - index;
-            accumulator += value * lengthArray;
-            return Number(accumulator);
-        }, 0);
-
-        return result;
-    };
 };
 
+CPFValidator.prototype.validCleanCpf = function (cleanCPF) {
+    if(typeof cleanCPF === 'undefined') return false;
+    if(cleanCPF.length !== 11) return false;
+    if(this.sequenceCPF(cleanCPF)) return false;
+
+    const sliceCPF = this.sliceCPF(cleanCPF);
+
+    const calcDigit1 = this.calcCPF(sliceCPF);
+    const digit1 = this.digitCalc(calcDigit1);
+    const calcDigit2 = this.calcCPF(sliceCPF + digit1);
+    const digit2 = this.digitCalc(calcDigit2);
+
+    const newCpf = sliceCPF + digit1 + digit2;
+
+    return newCpf === cleanCPF;
+};
+
+CPFValidator.prototype.sliceCPF = function(cleanCPF) {
+    return cleanCPF.slice(0, -2);
+}
+
+CPFValidator.prototype.cleanCPF = function (cpf) {
+    return cpf.replace(/\D+/g, '');
+}
+
+CPFValidator.prototype.calcCPF = function (sliceCPF) {
+    const arrayCPF = Array.from(sliceCPF);
+    
+    const result = arrayCPF.reduce((accumulator, value, index, array) => {
+        const lengthArray = (array.length + 1) - index;
+        accumulator += value * lengthArray;
+        return accumulator;
+    }, 0);
+
+    return result;
+};
+
+CPFValidator.prototype.digitCalc = function (result) {
+    const calculatedDigit = 11 - (result % 11);
+    return (calculatedDigit <= 9) ? String(calculatedDigit) : '0';
+};
+
+CPFValidator.prototype.sequenceCPF = function (cpf) {
+    const sequence = cpf[0].repeat(cpf.length);
+    return sequence === cpf;
+}
+
+
 const cpf = '705.484.450-52';
+// const cpf = '111.111.111-11';
 
 const cpfValidator = new CPFValidator(cpf);
 cpfValidator.init();
