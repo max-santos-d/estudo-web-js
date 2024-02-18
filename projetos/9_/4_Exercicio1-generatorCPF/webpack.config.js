4dcdc4de78b691989f256b31a4344dfff080b404
Max Santos <max.santosdev@gmail.com>
2024-02-18 03:24:03 -0300

    Inicialização do Exercicio de criação de CPF com Webpack

---
+++ projetos/9_/4_Exercicio1-generatorCPF/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path'); // CommonJs
+
+module.exports = {
+    mode: 'development', // modo quando em desenvolvimento, quando for para produção utilizar 'production'
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist', 'assets', 'js')
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    },
+    devtool: 'source-map'
+};
